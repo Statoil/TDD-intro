c8a6820bdecd146fbe58db1860a4821ff42ee94c
Kristian Flikka <kristian.flikka@gmail.com>
2016-04-01 14:18:53 +0200

    Added a couple of elements to the driver, and renamed files to smallcaps

---
+++ Step-2/driver.py
@@ -0,0 +1,4 @@
+
+class Driver(object):
+    def __init__(self, name=None):
+        self.name = name
---
+++ Step-2/drivertest.py
@@ -0,0 +1,19 @@
+# Task: Create tests that check a Car object, sets and checks name, age and gender
+import unittest
+from driver import Driver
+
+class DriverTest(unittest.TestCase):
+    def test_create_driver(self):
+        driver = Driver()
+        self.assertIsNotNone(driver, "Driver should not be None")
+
+    def test_create_with_constructor(self):
+        driver = Driver("Nils")
+        self.assertEqual(driver.name, "Nils", "Name should be Nils")
+
+    def test_set_name(self):
+        driver = Driver()
+        driver.name = "Birger"
+        self.assertEqual(driver.name, "Birger", "Name should equal Birger")
+
+
